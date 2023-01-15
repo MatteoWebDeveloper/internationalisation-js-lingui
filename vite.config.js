d0a106fdaa59cac2963e3e63ef0b89dce7bb8e77
MatteoWebDeveloper <matteowebdesigner@googlemail.com>
2023-01-15 22:25:47 +0000

    tested with preact

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import macrosPlugin from 'vite-plugin-babel-macros';
-import react from '@vitejs/plugin-react';
+import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [macrosPlugin(), react()],
+  plugins: [macrosPlugin(), preact()],
 });
